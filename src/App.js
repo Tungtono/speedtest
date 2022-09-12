5525cc4d8c5d11e11646e94b4d12242d294136e5
Joe <uehtung@gmail.com>
2022-09-12 16:40:17 -0400

    done UI

--- src/App.js
+++ src/App.js
@@ -184,6 +184,7 @@ const App = () => {
       ...prevState,
       down100Kb: stats.median(testDown100k).toFixed(2),
     }));
+    console.log(testDown100k);
     setProgress((prevState) => ({
       ...prevState,
       down100Kb: true,
@@ -238,7 +239,7 @@ const App = () => {
     ];
     setSpeed((prevState) => ({
       ...prevState,
-      downSpeed: stats.quartile(downloadTests, 0.9).toFixed(2),
+      downSpeed: stats.quartile(downloadTests, 0.75).toFixed(2),
     }));
     setProgress((prevState) => ({
       ...prevState,
@@ -259,6 +260,29 @@ const App = () => {
     }));
   };
 
+  const handleRetest = () => {
+    setSpeed({
+      down100Kb: 0,
+      down1Mb: 0,
+      down10Mb: 0,
+      down25Mb: 0,
+      down100Mb: 0,
+      downSpeed: 0,
+      upSpeed: 0,
+    })
+    setProgress({
+      latency: false,
+      jitter: false,
+      down100Kb: false,
+      down1Mb: false,
+      down10Mb: false,
+      down25Mb: false,
+      down100Mb: false,
+      downAll: false,
+      upAll: false,
+    })
+    speedTest()
+  }
   useEffect(() => {
     fetchCdnCgiTrace();
     speedTest();
@@ -266,32 +290,404 @@ const App = () => {
   }, []);
 
   return (
-    <div className="text-center">
+    <div className="flex flex-col justify-center items-center">
+      <div className="lg:w-2/3 w-screen mt-12 text-center">
+        <h1 className="text-4xl font-bold">Your Internet speed is</h1>
+        {progress.downAll ? (
+          <div className="flex justify-center gap-2 h-48">
+            <p className="text-9xl font-bold my-auto">
+              {Math.floor(speed.downSpeed)}
+            </p>
+            <div className="flex flex-col justify-center items-start gap-2">
+              <p className="text-3xl font-medium">
+                .{speed.downSpeed.toString().split(".")[1]}Mbps
+              </p>
+              <button onClick={() => handleRetest()} className="bg-green-600 rounded-lg flex pr-3 pl-2 py-1.5 gap-1">
+                <svg
+                  className="m-auto"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.1459 11.0499L12.9716 9.05752L15.3462 8.84977C14.4471 7.98322 13.2242 7.4503 11.8769 7.4503C9.11547 7.4503 6.87689 9.68888 6.87689 12.4503C6.87689 15.2117 9.11547 17.4503 11.8769 17.4503C13.6977 17.4503 15.2911 16.4771 16.1654 15.0224L18.1682 15.5231C17.0301 17.8487 14.6405 19.4503 11.8769 19.4503C8.0109 19.4503 4.87689 16.3163 4.87689 12.4503C4.87689 8.58431 8.0109 5.4503 11.8769 5.4503C13.8233 5.4503 15.5842 6.24474 16.853 7.52706L16.6078 4.72412L18.6002 4.5498L19.1231 10.527L13.1459 11.0499Z"
+                    fill="white"
+                  />
+                </svg>
+                <p className="text-white">retest</p>
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="h-48 flex justify-center items-center">
+            <div class="px-4 font-semibold text-4xl text-green-600">
+              ...doing magic
+            </div>
+            <div class="flex h-6 w-6">
+              <div class="animate-ping absolute h-6 w-6 rounded-full bg-green-600 opacity-75"></div>
+              <div class="relative rounded-full h-6 w-6 bg-green-600"></div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex justify-center flex-wrap lg:w-2/3 w-full">
+        {progress.latency ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Latency</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{latency}</p>
+              <p className=" text-sm">ms</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Calculating latency...
+          </div>
+        )}
+        {progress.jitter ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Jitter</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{jitter}</p>
+              <p className=" text-sm">ms</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Calculating jitter...
+          </div>
+        )}
+        {progress.down100Kb ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Download tests with 100Kb size</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{speed.down100Kb}</p>
+              <p className=" text-sm">Mbps</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Running tests with 100kb size...
+          </div>
+        )}
+        {progress.down1Mb ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Download tests with 1MB size</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{speed.down1Mb}</p>
+              <p className=" text-sm">Mbps</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Running tests with 1Mb size...
+          </div>
+        )}
+        {progress.down10Mb ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Download tests with 10MB size</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{speed.down10Mb}</p>
+              <p className=" text-sm">Mbps</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Running tests with 10Mb size...
+          </div>
+        )}
+        {progress.down25Mb ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Download tests with 25MB size</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{speed.down25Mb}</p>
+              <p className=" text-sm">Mbps</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Running tests with 25Mb size...
+          </div>
+        )}
+        {progress.down100Mb ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Download tests with 100MB size</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{speed.down100Mb}</p>
+              <p className=" text-sm">Mbps</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Running tests with 100Mb size...
+          </div>
+        )}
+        {progress.upAll ? (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+              </svg>
+            </div>
+            <p>Upload speed</p>
+            <div className="ml-auto flex">
+              <p className=" text-3xl font-semibold">{speed.upSpeed}</p>
+              <p className=" text-sm">Mbps</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Running upload tests...
+          </div>
+        )}
+      </div>
       <p>Your IP is: {ip}</p>
       <p>Fetching from the nearest server: {city}</p>
-      <p>Latency: {progress.latency ? `${latency}ms` : `running tests`}</p>
-      <p>Jitter: {progress.jitter ? `${jitter}ms` : `running tests`}</p>
-      <p>
-        100kb: {progress.down100Kb ? `${speed.down100Kb}Mbps` : `running tests`}
-      </p>
-      <p>1Mb: {progress.down1Mb ? `${speed.down1Mb}Mbps` : `running tests`}</p>
-      <p>
-        10Mb: {progress.down10Mb ? `${speed.down10Mb}Mbps` : `running tests`}
-      </p>
-      <p>
-        25Mb: {progress.down25Mb ? `${speed.down25Mb}Mbps` : `running tests`}
-      </p>
-      <p>
-        100Mb: {progress.down100Mb ? `${speed.down100Mb}Mbps` : `running tests`}
-      </p>
-      <p>
-        Overall Down Speed:{" "}
-        {progress.downAll ? `${speed.downSpeed}Mbps` : `running tests`}
-      </p>
-      <p>
-        Overall Up Speed:{" "}
-        {progress.upAll ? `${speed.upSpeed}Mbps` : `running tests`}
-      </p>
     </div>
   );
 };
