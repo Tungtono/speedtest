03168e7a7adff669aa1784829b0500c0eb6abfda
Joe <uehtung@gmail.com>
2022-09-12 10:35:36 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ const App = () => {
   const [ip, setIp] = useState("");
   const [latency, setLatency] = useState(0);
   const [jitter, setJitter] = useState(0);
-  const [speed100Kb, setSpeed100kb] = useState(0);
-  const [speed1Mb, setSpeed1Mb] = useState(0);
-  const [speed10Mb, setSpeed10Mb] = useState(0);
-  const [speed25Mb, setSpeed25Mb] = useState(0);
-  const [speed100Mb, setSpeed100Mb] = useState(0);
-  const [downloadSpeed, setDownloadSpeed] = useState(0);
-  const [uploadSpeed, setUploadSpeed] = useState(0);
+  const [speed, setSpeed] = useState({
+    down100Kb: 0,
+    down1Mb: 0,
+    down10Mb: 0,
+    down25Mb: 0,
+    down100Mb: 0,
+    downSpeed: 0,
+    upSpeed: 0,
+  });
 
   const fetchAllServerLoc = async () => {
     const response = await fetch("https://speed.cloudflare.com/locations");
@@ -35,8 +37,8 @@ const App = () => {
     const data = parseCdnCgiTrace(rawData);
     setIp(data.ip);
     const allServers = await fetchAllServerLoc();
-    const rightObj = allServers.find((item) => item.iata === data.colo);
-    setCity(rightObj.city);
+    const myServer = allServers.find((item) => item.iata === data.colo);
+    setCity(myServer.city);
   };
 
   const request = (options, data = "") => {
@@ -57,7 +59,7 @@ const App = () => {
         res.on("end", () => {
           resultData.ended = performance.now();
           resultData.serverTiming = parseFloat(
-            res.headers["server-timing"].slice(22)
+            res.headers["server-timing"].split("=")[1]
           );
           resolve(resultData);
         });
@@ -118,8 +120,7 @@ const App = () => {
     return {
       latency: stats.median(measurements),
       jitter: stats.jitter(measurements),
-    }
-
+    };
   };
 
   const measureDownload = async (bytes, iterations) => {
@@ -160,19 +161,34 @@ const App = () => {
     setJitter(ping.jitter.toFixed(2));
 
     const testDown100k = await measureDownload(101000, 10);
-    setSpeed100kb(stats.median(testDown100k).toFixed(2));
+    setSpeed((prevState) => ({
+      ...prevState,
+      down100Kb: stats.median(testDown100k).toFixed(2),
+    }));
 
     const testDown1m = await measureDownload(1001000, 8);
-    setSpeed1Mb(stats.median(testDown1m).toFixed(2));
+    setSpeed((prevState) => ({
+      ...prevState,
+      down1Mb: stats.median(testDown1m).toFixed(2),
+    }));
 
     const testDown10m = await measureDownload(10001000, 6);
-    setSpeed10Mb(stats.median(testDown10m).toFixed(2));
+    setSpeed((prevState) => ({
+      ...prevState,
+      down10Mb: stats.median(testDown10m).toFixed(2),
+    }));
 
     const testDown25m = await measureDownload(25001000, 4);
-    setSpeed25Mb(stats.median(testDown25m).toFixed(2));
+    setSpeed((prevState) => ({
+      ...prevState,
+      down25Mb: stats.median(testDown25m).toFixed(2),
+    }));
 
     const testDown100m = await measureDownload(100001000, 1);
-    setSpeed100Mb(stats.median(testDown100m).toFixed(2));
+    setSpeed((prevState) => ({
+      ...prevState,
+      down100Mb: stats.median(testDown100m).toFixed(2),
+    }));
 
     const downloadTests = [
       ...testDown100k,
@@ -181,13 +197,19 @@ const App = () => {
       ...testDown25m,
       ...testDown100m,
     ];
-    setDownloadSpeed(stats.quartile(downloadTests, 0.9).toFixed(2));
+    setSpeed((prevState) => ({
+      ...prevState,
+      downSpeed: stats.quartile(downloadTests, 0.9).toFixed(2),
+    }));
 
     const testUp1 = await measureUpload(11000, 10);
     const testUp2 = await measureUpload(101000, 10);
     const testUp3 = await measureUpload(1001000, 10);
     const uploadTests = [...testUp1, ...testUp2, ...testUp3];
-    setUploadSpeed(stats.quartile(uploadTests, 0.9).toFixed(2));
+    setSpeed((prevState) => ({
+      ...prevState,
+      upSpeed: stats.quartile(uploadTests, 0.9).toFixed(2),
+    }));
   };
 
   useEffect(() => {
@@ -202,13 +224,13 @@ const App = () => {
       <p>Fetching from the nearest server: {city}</p>
       <p>Latency: {latency}ms</p>
       <p>Jitter: {jitter}ms</p>
-      <p>100kb: {speed100Kb}Mbps</p>
-      <p>1Mb: {speed1Mb}Mbps</p>
-      <p>10Mb: {speed10Mb}Mbps</p>
-      <p>25Mb: {speed25Mb}Mbps</p>
-      <p>100Mb: {speed100Mb}Mbps</p>
-      <p>Overall Down Speed: {downloadSpeed}Mbps</p>
-      <p>Overall Up Speed: {uploadSpeed}Mbps</p>
+      <p>100kb: {speed.down100Kb}Mbps</p>
+      <p>1Mb: {speed.down1Mb}Mbps</p>
+      <p>10Mb: {speed.down10Mb}Mbps</p>
+      <p>25Mb: {speed.down25Mb}Mbps</p>
+      <p>100Mb: {speed.down100Mb}Mbps</p>
+      <p>Overall Down Speed: {speed.downSpeed}Mbps</p>
+      <p>Overall Up Speed: {speed.upSpeed}Mbps</p>
     </div>
   );
 };
