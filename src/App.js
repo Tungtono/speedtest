3f4c3574be67b8ccac1532c1f21bcde546877e91
Joe <uehtung@gmail.com>
2022-09-13 03:27:13 -0400

    add description

--- src/App.js
+++ src/App.js
@@ -35,44 +35,45 @@ const App = () => {
     {
       name: "latency",
       title: "latency",
-      description: "multiple tests to calculate round trip time (RTT)"
+      description: "multiple tests to calculate round trip time (RTT)",
     },
     {
       name: "jitter",
       title: "jitter",
-      description: "average difference between consecutive latency measurements"
+      description:
+        "average difference between consecutive latency measurements",
     },
     {
       name: "down100Kb",
       title: "flyweight",
-      description: "multiple download tests with 100Bk file size"
+      description: "multiple download tests with 100Bk file size",
     },
     {
       name: "down1Mb",
       title: "lightweight",
-      description: "multiple download tests with 1Mb file size"
+      description: "multiple download tests with 1Mb file size",
     },
     {
       name: "down10Mb",
       title: "welterweight",
-      description: "multiple download tests with 10Mb file size"
+      description: "multiple download tests with 10Mb file size",
     },
     {
       name: "down25Mb",
       title: "middleweight",
-      description: "multiple download tests with 25Mb file size"
+      description: "multiple download tests with 25Mb file size",
     },
     {
       name: "down100Mb",
       title: "heavyweight",
-      description: "multiple download tests with 100Mb file size"
+      description: "multiple download tests with 100Mb file size",
     },
     {
       name: "upOverall",
       title: "upload",
-      description: "multiple upload tests with various sizes"
+      description: "multiple upload tests with various sizes",
     },
-  ]
+  ];
 
   const getBackgroundInfo = async () => {
     const response = await fetch("https://www.cloudflare.com/cdn-cgi/trace");
@@ -215,8 +216,8 @@ const App = () => {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="lg:w-2/3 w-screen mt-12 text-center">
+    <div className="flex flex-col justify-center items-center mt-12 mb-2">
+      <div className="lg:w-2/3 w-screen text-center">
         <h1 className="text-4xl font-bold">Your Internet speed is</h1>
         {progress.downOverall ? (
           <div className="flex justify-center gap-2 h-48">
@@ -261,59 +262,79 @@ const App = () => {
         )}
       </div>
       <div className="flex justify-center flex-wrap lg:w-2/3 w-full">
-        {tests.map(item => {
-            return (
-              <>
+        {tests.map((item) => {
+          return (
+            <>
               {progress[item.name] ? (
-              <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
-                <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="white"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                  </svg>
+                <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+                  <div className="flex flex-col">
+                    <div className="flex">
+                      <div className="flex justify-center items-center bg-green-600 rounded-full h-5 w-5 text-xl mr-3">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="white"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                        </svg>
+                      </div>
+                      <p>{item.title} result</p>
+                    </div>
+                    <div className="text-[0.65rem] mt-2 font-normal">
+                      {item.description}
+                    </div>
+                  </div>
+                  <div className="ml-auto flex pl-4">
+                    <p className=" text-3xl font-semibold">
+                      {metrics[item.name]}
+                    </p>
+                    <p className=" text-sm">
+                      {item.name === "latency" || item.name === "jitter"
+                        ? "ms"
+                        : "Mbps"}
+                    </p>
+                  </div>
                 </div>
-                <p>{item.title} result</p>
-                <div className="ml-auto flex">
-                  <p className=" text-3xl font-semibold">{metrics[item.name]}</p>
-                  <p className=" text-sm">{(item.name === "latency" || item.name === "jitter") ? "ms" : "Mbps"}</p>
+              ) : (
+                <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
+                  <div className="flex flex-col">
+                    <div className="flex">
+                      <svg
+                        className="animate-spin mr-3 h-5 w-5 text-green-600"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                      <p>Running {item.title} tests...</p>
+                    </div>
+                    <div className="text-[0.65rem] mt-2 font-normal">
+                      {item.description}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ) : (
-              <div className="flex border-t border-gray-500 items-center px-4 py-4 font-medium lg:w-96 w-full mx-4 h-24">
-                <svg
-                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-                Running {item.title} tests...
-              </div>
-            )}
-              </>
-            )
+              )}
+            </>
+          );
         })}
       </div>
-      <p className="text-sm">Your IP is: {ip}</p>
-      <p className="text-sm">Fetching from the nearest server: {city}</p>
+      <p className="text-sm mt-2">Your IP is: {ip}</p>
+      <p className="text-sm mt-2">Fetching from the nearest server: {city}</p>
     </div>
   );
 };
